3d762d3c7c307b2ac837fedaacc72c14a8b1d613
Siim Schults <siim.schults@gmail.com>
2018-09-05 14:03:54 +0300

    using arrow functions

--- src/wordcloud.js
+++ src/wordcloud.js
@@ -4,23 +4,19 @@ import d3Cloud from 'd3-cloud';
 export class WordCloud {
 
   constructor(words) {
-    const self = this;
+    const w = window.innerWidth, h = window.innerHeight;
     this.fill = scaleOrdinal(schemeCategory10);
-    let w = window.innerWidth, h = window.innerHeight;
-    this.svg;
-    this.vis;
-    this.scale;
     this.fontSize = scaleSqrt().range([10, 100]).domain([+words[words.length - 1].value || 1, +words[0].value]);
     this.layout = d3Cloud().timeInterval(Infinity).size([w, h])
-    .fontSize(function (d) {
-      return self.fontSize(+d.value);
+    .fontSize((d) => {
+      return this.fontSize(+d.value);
     })
-    .text(function (d) {
+    .text((d) => {
       return d.key;
     })
     .font('impact')
     .spiral('archimedean')
-    .on("end", function(data, bounds) {self.draw.call(self, data, bounds)});
+    .on("end", this.draw);
 
     this.svg = select("#vis").append("svg").attr("width", w).attr("height", h);
     this.vis = this.svg.append("g").attr("transform", "translate(" + [w / 2, h / 2] + ")");
@@ -28,6 +24,10 @@ export class WordCloud {
     this.layout.words(words);
     this.layout.start().start();
 
+    this.updateOnToWindowResize();
+  }
+
+  updateOnToWindowResize() {
     if (window.attachEvent) {
       window.attachEvent('onresize', this.update);
     }
@@ -39,10 +39,9 @@ export class WordCloud {
   update = () => {
     console.log('UPDATE');
     this.layout.stop().start();
-  }
+  };
 
-  draw(data, bounds) {
-    const self = this;
+  draw = (data, bounds) => {
     console.log('DRAW', 'data example', data[0], this);
     let w = window.innerWidth,
       h = window.innerHeight;
@@ -56,37 +55,37 @@ export class WordCloud {
       h / Math.abs(bounds[0].y - h / 2)) / 2 : 1;
 
     let text = this.vis.selectAll("text")
-    .data(data, function (d) {
+    .data(data, (d) => {
       return d.text.toLowerCase();
     });
     text
     .transition()
     .duration(1000)
-    .attr("transform", function (d) {
+    .attr("transform", (d) => {
       return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
     })
-    .style("font-size", function (d) {
+    .style("font-size", (d) => {
       return d.size + "px";
     });
     text.enter().append("text")
     .attr("text-anchor", "middle")
-    .attr("transform", function (d) {
+    .attr("transform", (d) => {
       return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
     })
-    .style("font-size", function (d) {
+    .style("font-size", (d) => {
       return d.size + "px";
     })
     .style("opacity", 1);
-    text.style("font-family", function (d) {
+    text.style("font-family", (d) => {
       return d.font;
     })
-    .style("fill", function (d) {
-      return self.fill(d.text.toLowerCase());
+    .style("fill", (d) => {
+      return this.fill(d.text.toLowerCase())
     })
-    .text(function (d) {
+    .text((d) => {
       return d.text;
     });
 
     this.vis.transition().attr("transform", "translate(" + [w / 2, h / 2] + ")scale(" + this.scale + ")");
-  }
+  };
 }
